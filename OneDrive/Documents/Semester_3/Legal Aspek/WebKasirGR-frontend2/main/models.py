2a0357090f5670cf4cdfdb3b642e121335dd7c3e
RIfat <143817676+Rifatkim@users.noreply.github.com>
2025-11-23 19:10:11 +0700

    Delete OneDrive/Documents/Semester_3/Legal Aspek/WebKasirGR-frontend2 directory

--- OneDrive/Documents/Semester_3/Legal Aspek/WebKasirGR-frontend2/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Merchant(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    store_name = models.CharField(max_length=100)
-
-# Create your models here.
